dec9833270682ff705ace99357a760725a36b71a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-07-19 13:44:33 -0300

    initial commit

---
+++ server.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'rack/handler/puma'
+require 'csv'
+
+get '/tests' do
+  ### Example
+  # [
+  #   [cpf, name, date],
+  #   [123, 'Leandro', 2022-01-01'],
+  #   [456, 'Ana', 2022-01-02'],
+  #   [789, 'Maria', 2022-01-03']
+  # ]
+  #
+  rows = CSV.read('app/data.csv', col_sep: ';')
+
+  ### Example
+  # [cpf, name, date]
+  columns = rows.shift
+
+  ### Example
+  # From:  [123, 'Leandro', 2022-01-01']
+  # To: { cpf: 123, name: 'Leandro', date: '2022-01-01' }
+  rows.map do |row|
+    row.each_with_object({}).with_index do |(cell, acc), idx|
+      column = columns[idx]
+      acc[column] = cell
+    end
+  end.to_json
+end
