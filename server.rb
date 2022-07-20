6feb4a93ae4caf4e3e31073fc3c40d0fc6c6be13
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-07-20 13:44:08 -0300

    refactor server.rb

--- server.rb
+++ server.rb
@@ -3,30 +3,20 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'rack/handler/puma'
-require 'csv'
+require './models/medical_exam'
+require './services/import_from_csv'
 
-get '/tests' do
-  ### Example
-  # [
-  #   [cpf, name, date],
-  #   [123, 'Leandro', 2022-01-01'],
-  #   [456, 'Ana', 2022-01-02'],
-  #   [789, 'Maria', 2022-01-03']
-  # ]
-  #
-  rows = CSV.read('app/data.csv', col_sep: ';')
+class Server < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
 
-  ### Example
-  # [cpf, name, date]
-  columns = rows.shift
+  set :bind, '0.0.0.0'
+  set :port, 3000
 
-  ### Example
-  # From:  [123, 'Leandro', 2022-01-01']
-  # To: { cpf: 123, name: 'Leandro', date: '2022-01-01' }
-  rows.map do |row|
-    row.each_with_object({}).with_index do |(cell, acc), idx|
-      column = columns[idx]
-      acc[column] = cell
-    end
-  end.to_json
+  get '/tests' do
+    MedicalExam.all.to_json(except: %i[id created_at updated_at])
+  end
+
+  run! if app_file == $PROGRAM_NAME
 end
