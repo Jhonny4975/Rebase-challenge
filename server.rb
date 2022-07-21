62d1987bba79ceec24daac9e895d49f788935e5f
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-07-21 19:50:01 -0300

    add get exam details endpoint

--- server.rb
+++ server.rb
@@ -5,6 +5,7 @@ require 'sinatra/activerecord'
 require 'rack/handler/puma'
 require './models/medical_exam'
 require './services/csv_data_handler'
+require './serializers/exam_serializer'
 
 class Server < Sinatra::Base
   configure :production, :development do
@@ -30,8 +31,20 @@ class Server < Sinatra::Base
     { message: 'Success! Registration will be carried out soon.' }.to_json
   end
 
+  get '/tests/:token' do
+    exams = MedicalExam.where(exam_result_token: params[:token])
+
+    return 404 if exams.empty?
+
+    ExamSerializer.details(exams).to_json
+  end
+
   error 412 do
     { error: 'Could not continue, unrecognized file type.' }.to_json
+  end
+
+  error 404 do
+    { error: 'Could not find exam with given data.' }.to_json
   end
 
   run! if app_file == $PROGRAM_NAME
