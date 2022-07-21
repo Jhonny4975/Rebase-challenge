b3566ad542142ceb371db72d209618f52fbaaa50
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-07-21 15:30:01 -0300

    setup sidekiq

---
+++ config/sidekiq.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+require_relative '../workers/import'
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379/0' }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379/0' }
+end
